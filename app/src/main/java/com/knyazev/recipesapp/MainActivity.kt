d9bdbeb31b27219fc9530f1c2bdfad4136b2fc1e
Artur <ArtSoull@yandex.ru>
2024-08-10 18:48:20 +0500

    RA-03

--- app/src/main/java/com/knyazev/recipesapp/MainActivity.kt
+++ app/src/main/java/com/knyazev/recipesapp/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.knyazev.recipesapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.knyazev.recipesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
