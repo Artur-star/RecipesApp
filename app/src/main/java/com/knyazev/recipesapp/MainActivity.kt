5475a73f7a6afa966c90800a7a30f346d1e32d5c
Artut <ArtSoull@yandex.ru>
2024-08-23 11:17:59 +0500

    RA-04 Create first fragment with categories

--- app/src/main/java/com/knyazev/recipesapp/MainActivity.kt
+++ app/src/main/java/com/knyazev/recipesapp/MainActivity.kt
@@ -2,18 +2,19 @@ package com.knyazev.recipesapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.knyazev.recipesapp.databinding.ActivityMainBinding
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
 
-    private var _binding: ActivityMainBinding? = null
-    private val binding: ActivityMainBinding
-        get() = _binding
-            ?: throw IllegalArgumentException("Binding for ActivityMainBinding must not be null")
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add(R.id.mainContainer, CategoriesListFragment())
+            }
+        }
     }
 }
