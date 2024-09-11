0055f21f0e9e6883ffba167c43814823e9d00eef
Artut <ArtSoull@yandex.ru>
2024-09-11 16:23:54 +0500

    RA-08 Implementation recycler view with list categories

--- app/src/main/java/com/knyazev/recipesapp/CategoriesListFragment.kt
+++ app/src/main/java/com/knyazev/recipesapp/CategoriesListFragment.kt
@@ -1,10 +1,10 @@
 package com.knyazev.recipesapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.knyazev.recipesapp.databinding.FragmentListCategoriesBinding
 
 class CategoriesListFragment : Fragment() {
@@ -21,8 +21,18 @@ class CategoriesListFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecycler()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun initRecycler() {
+        val listAdapter = CategoriesListAdapter(STUB.getCategories())
+        binding.rvCategories.adapter = listAdapter
     }
 }
