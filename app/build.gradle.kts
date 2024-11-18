30f77509cb2f876f7711c89fa70bd113b0fcc65a
Artut <ArtSoull@yandex.ru>
2024-11-18 19:51:30 +0500

    RA-29 Add save args plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     id(libs.plugins.kotlin.parcelize.get().pluginId)
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
@@ -38,7 +39,6 @@ android {
     buildFeatures {
         viewBinding = true
     }
-
 }
 
 dependencies {
